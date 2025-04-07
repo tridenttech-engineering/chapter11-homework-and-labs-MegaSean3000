95daa800ca9d474e72c3bc9f56603f7222193587
MegaSean3000 <sfp1216@gmail.com>
2025-04-07 15:02:20 -0400

    Update hw11-21.cpp

--- hw11-21.cpp
+++ hw11-21.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
+int main() {
+    int stockPrices[] = { 25, 65, 23, 45, 66, 77, 88, 43, 335, 65, 656 };
+    int size = sizeof(stockPrices) / sizeof(stockPrices[0]);
 
-{
+    int sum = 0;
 
-int stockPrices[] = { 25, 65, 23, 45, 66, 77, 88, 43, 335, 65, 656 };
+    for (int i = 0; i < size; ++i) {
+        sum += stockPrices[i];
+    }
 
-int sum = 0;
+    float average = static_cast<float>(sum) / size;
 
-for (int i = 0; i < (sizeof(stockPrices) / sizeof(*stockPrices)); i++)
+    cout << "The average value for the stock prices is " << average << endl;
 
-{
-
-sum = sum + stockPrices[i];
-
-}
-
-float avg= sum / (sizeof(stockPrices) / sizeof(*stockPrices));
-
-cout<< "The average value for the stock prices are " << avg;
-
-getchar();
-
+    return 0;
 }
