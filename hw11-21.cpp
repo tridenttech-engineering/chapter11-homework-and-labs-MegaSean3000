cf81abe959c089132223fe0394d84638b926d232
MegaSean3000 <sfp1216@gmail.com>
2025-04-07 14:46:09 -0400

    Update hw11-21.cpp

--- hw11-21.cpp
+++ hw11-21.cpp
@@ -1,19 +1,35 @@
-//Introductory21.cpp - Displays the average stock price
-//Created/revised by <your name> on <current date>
-
-#include <iostream>
-#include <iomanip>
-using namespace std;
-
-int main()
-{
-	double prices[10] = {96.5, 100.5, 100.5, 100.5, 99, 99, 99, 100, 98.5, 98.9};
-	double total = 0.0;
-	double average = 0.0;
-
-
-	cout << fixed << setprecision(2);
-	cout << "Average stock price: $" << average << endl;
-	return 0;
-}	//end of main function
-
+  //Lab11-5.cpp
+  //Stores averages in a one-dimensional array
+  //Created/revised by <your name> on <current date>
+ 
+  #include <iostream>
+  using namespace std;
+ 
+  int main()
+  { 
+    //declare arrays
+    double midterms[5] = {0.0};
+    double finals[5] = {0.0};
+	  double averages[5] = {0.0};
+ 
+    //get exam score 00016   
+	  for (int x = 0; x < 5; x += 1)
+     {
+         cout << "Midterm exam score for student " 
+             << x + 1 << ": ";
+         cin >> midterms[x];
+         cout << "Final exam score for student "
+              << x + 1 << ": ";
+         cin >> finals[x];
+         cout << endl;
+         //calculate and assign average
+         averages[x] = (midterms[x] + finals[x]) / 2;
+     }  //end for
+     //display contents of array
+     cout << endl;
+     for (int y = 0; y < 5; y += 1)
+         cout << "Student " << y + 1 << " average: " 
+             << averages[y] << endl;
+     //end for
+     return 0;
+  }  //end of main function
