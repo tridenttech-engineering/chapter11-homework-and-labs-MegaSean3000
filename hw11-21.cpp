79ad3287ea4afcb6339b42fd4ce995a20f191ac7
MegaSean3000 <sfp1216@gmail.com>
2025-04-07 15:30:39 -0400

    Update hw11-21.cpp

--- hw11-21.cpp
+++ hw11-21.cpp
@@ -1,16 +1,27 @@
+//Introductory21.cpp - Displays the average stock price
+//Created/revised by <Your Name> on <Current Date>
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
 int main()
-
 {
-double prices[10] = {96.5, 100.5, 100.5, 100.5, 99, 99, 99, 100, 98.5, 98.9};
-double total = 0.0;
-double average = 0.0;
+    double prices[10] = {96.5, 100.5, 100.5, 100.5, 99, 99, 99, 100, 98.5, 98.9};
+    double total = 0.0;
+    double average = 0.0;
 
-cout << fixed << setprecision(2);
-cout << "Average stock price: $" << average << endl;
-return 0;
+    // Use a for loop to calculate the total
+    for (int i = 0; i < 10; i++) {
+        total += prices[i];
+    }
 
-} //end of main function
+    // Calculate the average
+    average = total / 10;
+
+    // Output the result
+    cout << fixed << setprecision(2);
+    cout << "Average stock price: $" << average << endl;
+
+    return 0;
+}    //end of main function
