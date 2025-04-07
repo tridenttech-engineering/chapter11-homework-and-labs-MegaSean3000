5f83309e6abf1a57a59db804736ed49f20ac8e60
MegaSean3000 <sfp1216@gmail.com>
2025-04-07 14:53:37 -0400

    Update hw11-21.cpp

--- hw11-21.cpp
+++ hw11-21.cpp
@@ -1,35 +1,27 @@
-  //Lab11-5.cpp
-  //Stores averages in a one-dimensional array
-  //Created/revised by <your name> on <current date>
- 
-  #include <iostream>
-  using namespace std;
- 
-  int main()
-  { 
-    //declare arrays
-    double midterms[5] = {0.0};
-    double finals[5] = {0.0};
-	  double averages[5] = {0.0};
- 
-    //get exam score 00016   
-	  for (int x = 0; x < 5; x += 1)
-     {
-         cout << "Midterm exam score for student " 
-             << x + 1 << ": ";
-         cin >> midterms[x];
-         cout << "Final exam score for student "
-              << x + 1 << ": ";
-         cin >> finals[x];
-         cout << endl;
-         //calculate and assign average
-         averages[x] = (midterms[x] + finals[x]) / 2;
-     }  //end for
-     //display contents of array
-     cout << endl;
-     for (int y = 0; y < 5; y += 1)
-         cout << "Student " << y + 1 << " average: " 
-             << averages[y] << endl;
-     //end for
-     return 0;
-  }  //end of main function
+#include <iostream>
+
+using namespace std;
+
+void main()
+
+{
+
+int stockPrices[] = { 25, 65, 23, 45, 66, 77, 88, 43, 335, 65, 656 };
+
+int sum = 0;
+
+for (int i = 0; i < (sizeof(stockPrices) / sizeof(*stockPrices)); i++)
+
+{
+
+sum = sum + stockPrices[i];
+
+}
+
+float avg= sum / (sizeof(stockPrices) / sizeof(*stockPrices));
+
+cout<< "The average value for the stock prices are " << avg;
+
+getchar();
+
+}
